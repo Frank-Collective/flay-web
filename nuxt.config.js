65f12eb47c1bb0f7861f35ba9435994ada097217
Brendan Sheehan <feemcgill@gmail.com>
2021-03-03 16:18:20 -0800

    change back to spa mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,9 @@ export default {
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: 'static',
+  mode: 'spa',
+  //target: 'static',
+  ssr: false,
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
