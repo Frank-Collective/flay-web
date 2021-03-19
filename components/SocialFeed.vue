dad87b3dc26dff93b8fd46be5e6d589b7580db53
Brendan Sheehan <feemcgill@gmail.com>
2021-03-19 16:33:47 -0700

    test

--- components/SocialFeed.vue
+++ components/SocialFeed.vue
@@ -27,10 +27,10 @@ export default {
     }
   },
   mounted() {
-    this.getInstaFeed()
+    //this.getInstaFeed()
   },
   updated() {
-    this.getInstaFeed()
+    //this.getInstaFeed()
   },
   methods: {
     getInstaFeed() {
